ee5ca104b86e40b3b6591f8b9a899053754cfba4
Richard Lopez <rl3020@columbia.edu>
2021-10-05 03:37:46 +0000

    First attempt to update website

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,23 @@
 class Movie < ActiveRecord::Base
+  
+  def all_ratings 
+    ['G', 'PG', 'PG-13', 'R']
+  end
+  
+  def self.with_ratings(ratings_list)
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+  #  movies with those ratings
+  #  
+  # if ratings_list is nil, retrieve ALL movies
+    if ratings_list.nil?
+      return self.all
+    end
+    
+    if ratings_list.length > 0
+      where(ratings: ratings_list)
+    end
+    
+    
+  end
+  
 end
