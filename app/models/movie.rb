c0ec5c6671f38a60cbcf9a10184a0d4b5fd7683a
Richard Lopez <rl3020@columbia.edu>
2021-10-05 03:41:24 +0000

    fixed bug 'no all_ratings method'

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   
-  def all_ratings 
-    ['G', 'PG', 'PG-13', 'R']
+  def self.all_ratings 
+    return ['G', 'PG', 'PG-13', 'R']
   end
   
   def self.with_ratings(ratings_list)
